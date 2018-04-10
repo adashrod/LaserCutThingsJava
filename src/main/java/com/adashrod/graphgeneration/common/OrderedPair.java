4d801f8e203f090f67eea6966f8af949be6e4e65
Aaron Rodriguez <adashrod@gmail.com>
2018-04-10 17:47:04 -0400

    hashCode and equals for OrderedPair for HashMaps and such

--- src/main/java/com/adashrod/graphgeneration/common/OrderedPair.java
+++ src/main/java/com/adashrod/graphgeneration/common/OrderedPair.java
@@ -1,5 +1,7 @@
 package com.adashrod.graphgeneration.common;
 
+import java.util.Objects;
+
 /**
  * A container for two numeric coordinates
  * @author adashrod@gmail.com
@@ -16,4 +18,24 @@ public class OrderedPair<T extends Number> {
     public String toString() {
         return String.format("OrderedPair[x=%s, y=%s]", x, y);
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + Objects.hashCode(x);
+        result = prime * result + Objects.hashCode(y);
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object anObject) {
+        if (!(anObject instanceof OrderedPair)) {
+            return false;
+        }
+        final OrderedPair anOrderedPair = (OrderedPair) anObject;
+        return x.equals(anOrderedPair.x) && y.equals(anOrderedPair.y);
+    }
+
 }
